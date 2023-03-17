2b8e9bb3d155d5b3acceb73605bee0249b4c45d3
Steve Murphy <spmurphy@jacks.sdstate.edu>
2023-03-17 13:44:23 -0500

    Added Cart page and class

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
-
-
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import './data/globals.dart';
 import './pages/product_detail_page.dart';
 import './pages/products_overview_page.dart';
+import './pages/cart_page.dart';
+import './providers/cart_provider.dart';
 
 void main() => runApp(const MyApp());
 
@@ -13,27 +14,29 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'My Shop',
-      theme: ThemeData(
-        fontFamily: 'MinionPro',
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Globals.sdsuBlue,
-          primary: Globals.sdsuBlue,
-          secondary: Globals.sdsuYellow,
+    return ChangeNotifierProvider(
+      create: (context) => Cart(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'My Shop',
+        theme: ThemeData(
+          fontFamily: 'MinionPro',
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: Globals.sdsuBlue,
+            primary: Globals.sdsuBlue,
+            secondary: Globals.sdsuYellow,
+          ),
+          canvasColor: Globals.sdsuLightBlue,
         ),
-        canvasColor: Globals.sdsuLightBlue,
+        // home: const ProductsOverviewPage(),
+        initialRoute: ProductsOverviewPage.routeName,
+        routes: {
+          ProductsOverviewPage.routeName: (context) =>
+              const ProductsOverviewPage(),
+          ProductDetailPage.routeName: (context) => const ProductDetailPage(),
+          CartPage.routeName: (context) => const CartPage(),
+        },
       ),
-      // home: const ProductsOverviewPage(),
-      initialRoute: ProductsOverviewPage.routeName,
-      routes: {
-        ProductsOverviewPage.routeName:
-            (context) => const ProductsOverviewPage(),
-        ProductDetailPage.routeName:
-            (context) => const ProductDetailPage(),
-      },
     );
   }
 }
-
---
+++ lib/models/cart_item.dart
@@ -0,0 +1,11 @@
+class CartItem {
+  final String name;
+  final int quantity;
+  late double price;
+
+  CartItem({
+    required this.name,
+    required this.quantity,
+    required this.price,
+  });
+}
