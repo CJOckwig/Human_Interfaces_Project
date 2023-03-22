b8a3b557f1386480d482895c2f17a1d8b26441e2
Caleb Ockwig <caleb.ockwig@gmail.com>
2023-03-22 10:19:58 -0500

    added customer info page

--- lib/data/globals.dart
+++ lib/data/globals.dart
@@ -1,19 +1,25 @@
-
 // data/globals.dart
 
 import 'package:flutter/material.dart';
 
 class Globals {
   // SDSU Primary Colors
-  static const Color sdsuBlue         = Color.fromARGB(255,   0,  63, 220);
-  static const Color sdsuYellow       = Color.fromARGB(255, 255, 230,   0);
+  static const Color sdsuBlue = Color.fromARGB(255, 0, 63, 220);
+  static const Color sdsuYellow = Color.fromARGB(255, 255, 230, 0);
   // SDSU Secondary Colors
-  static const Color sdsuAthleticBlue = Color.fromARGB(255,   0,  71, 255);
-  static const Color sdsuNavy         = Color.fromARGB(255,   0,  21,  80);
-  static const Color sdsuDarkYellow   = Color.fromARGB(255, 212, 169,   0);
-  static const Color sdsuLightYellow  = Color.fromARGB(255, 252, 255, 168);
-  static const Color sdsuLightBlue    = Color.fromARGB(255, 201, 232, 255);
+  static const Color sdsuAthleticBlue = Color.fromARGB(255, 0, 71, 255);
+  static const Color sdsuNavy = Color.fromARGB(255, 0, 21, 80);
+  static const Color sdsuDarkYellow = Color.fromARGB(255, 212, 169, 0);
+  static const Color sdsuLightYellow = Color.fromARGB(255, 252, 255, 168);
+  static const Color sdsuLightBlue = Color.fromARGB(255, 201, 232, 255);
+
+  //https://www.schemecolor.com/mocha-latte.php
+  //I pulled this from this site. I coded it similar to how the example is.
+  //-CJO
+  static const Color coffeeDarkBrown = Color.fromARGB(255, 62, 0, 14);
+  static const Color coffeeWhite = Color.fromARGB(255, 244, 243, 248);
+  static const Color coffeeCream = Color.fromARGB(255, 232, 210, 185);
+  static const Color coffeeLightBrown1 = Color.fromARGB(255, 232, 210, 185);
+  static const Color coffeeLightBrown2 = Color.fromARGB(255, 206, 164, 122);
+  static const Color coffeeLightBrown3 = Color.fromARGB(255, 181, 128, 72);
 }
-
-
-
--- lib/pages/customer_info_page.dart
+++ lib/pages/customer_info_page.dart
@@ -1,5 +1,8 @@
 //CJOckwig Customer info page
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import '../data/globals.dart';
 
 class CustomerInfoPage extends StatelessWidget {
   const CustomerInfoPage({super.key});
@@ -7,13 +10,98 @@ class CustomerInfoPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var selectedOption;
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Customer Information'),
-      ),
-      body: const Center(
-        child: Text('Put customer info here pages/customer_info_page.dart'),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Customer Information'),
+          backgroundColor: Globals.coffeeDarkBrown,
+          shadowColor: Globals.coffeeCream,
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Padding(padding: EdgeInsets.all(5)),
+            SizedBox(width: 16),
+            Expanded(
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: 'First name',
+                  border: OutlineInputBorder(
+                    borderSide:
+                        BorderSide(color: Globals.coffeeDarkBrown, width: 2.0),
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: 'Last name',
+                  border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                    color: Globals.coffeeDarkBrown,
+                    width: 2.0,
+                  )),
+                ),
+              ),
+            ),
+            Expanded(
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                    color: Globals.coffeeDarkBrown,
+                    width: 2.0,
+                  )),
+                ),
+              ),
+            ),
+            Expanded(
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: 'Phone Number',
+                  border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                    color: Globals.coffeeDarkBrown,
+                    width: 2.0,
+                  )),
+                ),
+              ),
+            ),
+            DropdownButton(
+              items: const [
+                DropdownMenuItem(value: 1, child: Text('Brookings')),
+                DropdownMenuItem(value: 2, child: Text('Sioux Falls')),
+              ],
+              onChanged: (selectedOption) {
+                selectedOption = selectedOption;
+              },
+              value: selectedOption,
+            ),
+            ButtonBar(
+              alignment: MainAxisAlignment.center,
+              children: [
+                BackButton(
+                  onPressed: () {},
+                ),
+                TextButton(
+                  onPressed: () {},
+                  style: ButtonStyle(
+                    //foregroundColor: Globals.coffeeCream,
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18.0),
+                        side: BorderSide(color: Globals.coffeeDarkBrown),
+                      ),
+                    ),
+                  ),
+                  child: Text('proceed to payment screen'),
+                ),
+              ],
+            )
+          ],
+        ));
   }
 }
