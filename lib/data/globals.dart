ea78281fc22b2937e5046f811ec77e34b7defc6f
murphy-steve <spmurphy@jacks.sdstate.edu>
2023-03-31 10:06:43 -0500

    Color Theme added

--- lib/data/globals.dart
+++ lib/data/globals.dart
@@ -1,19 +1,19 @@
-
 // data/globals.dart
 
 import 'package:flutter/material.dart';
 
 class Globals {
+  // Caribou Coffee Colors
+  static const Color caribouBrown = Color.fromARGB(255, 49, 36, 24);
+  static const Color caribouBlue = Color.fromARGB(255, 121, 175, 188);
+  static const Color caribouLightGrey = Color.fromARGB(255, 246, 246, 246);
   // SDSU Primary Colors
-  static const Color sdsuBlue         = Color.fromARGB(255,   0,  63, 220);
-  static const Color sdsuYellow       = Color.fromARGB(255, 255, 230,   0);
+  static const Color sdsuBlue = Color.fromARGB(255, 0, 63, 220);
+  static const Color sdsuYellow = Color.fromARGB(255, 255, 230, 0);
   // SDSU Secondary Colors
-  static const Color sdsuAthleticBlue = Color.fromARGB(255,   0,  71, 255);
-  static const Color sdsuNavy         = Color.fromARGB(255,   0,  21,  80);
-  static const Color sdsuDarkYellow   = Color.fromARGB(255, 212, 169,   0);
-  static const Color sdsuLightYellow  = Color.fromARGB(255, 252, 255, 168);
-  static const Color sdsuLightBlue    = Color.fromARGB(255, 201, 232, 255);
+  static const Color sdsuAthleticBlue = Color.fromARGB(255, 0, 71, 255);
+  static const Color sdsuNavy = Color.fromARGB(255, 0, 21, 80);
+  static const Color sdsuDarkYellow = Color.fromARGB(255, 212, 169, 0);
+  static const Color sdsuLightYellow = Color.fromARGB(255, 252, 255, 168);
+  static const Color sdsuLightBlue = Color.fromARGB(255, 201, 232, 255);
 }
-
-
-
