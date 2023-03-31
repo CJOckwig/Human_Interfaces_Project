8a2b987808bf0b960473e755a3a81e21bfc549ef
Caleb Ockwig <caleb.ockwig@gmail.com>
2023-03-31 15:58:57 -0500

    added rough draft of base screen

--- lib/data/globals.dart
+++ lib/data/globals.dart
@@ -12,14 +12,4 @@ class Globals {
   static const Color sdsuDarkYellow = Color.fromARGB(255, 212, 169, 0);
   static const Color sdsuLightYellow = Color.fromARGB(255, 252, 255, 168);
   static const Color sdsuLightBlue = Color.fromARGB(255, 201, 232, 255);
-
-  //https://www.schemecolor.com/mocha-latte.php
-  //I pulled this from this site. I coded it similar to how the example is.
-  //-CJO
-  static const Color coffeeDarkBrown = Color.fromARGB(255, 62, 0, 14);
-  static const Color coffeeWhite = Color.fromARGB(255, 244, 243, 248);
-  static const Color coffeeCream = Color.fromARGB(255, 232, 210, 185);
-  static const Color coffeeLightBrown1 = Color.fromARGB(255, 232, 210, 185);
-  static const Color coffeeLightBrown2 = Color.fromARGB(255, 206, 164, 122);
-  static const Color coffeeLightBrown3 = Color.fromARGB(255, 181, 128, 72);
 }
