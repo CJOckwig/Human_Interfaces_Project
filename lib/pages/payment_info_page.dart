b3ca0d3156f1de1e0c1731e49102f3121c4ce93b
Caleb Ockwig <caleb.ockwig@gmail.com>
2023-04-01 13:47:10 -0500

    added more buttons and such to payment_info

--- lib/pages/payment_info_page.dart
+++ lib/pages/payment_info_page.dart
@@ -16,33 +16,35 @@ class PaymentInfoPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: const Icon(
-            Icons.home,
-          ),
-          onPressed: () => Navigator.pushNamed(
-            context,
-            ProductsOverviewPage.routeName,
-          ),
-        ),
-        title: const Text('Payment Information'),
-        actions: <Widget>[
-          IconButton(
+        appBar: AppBar(
+          leading: IconButton(
             icon: const Icon(
-              Icons.shopping_cart,
+              Icons.home,
             ),
             onPressed: () => Navigator.pushNamed(
               context,
-              CartPage.routeName,
+              ProductsOverviewPage.routeName,
             ),
           ),
-        ],
-      ),
-      body: Column(
-          //Padding(  padding: EdgeInsets.only(top: 10.0, left: 15.0),)
-          children: <Widget>[
-            /*
+          title: const Text('Payment Information'),
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(
+                Icons.shopping_cart,
+              ),
+              onPressed: () => Navigator.pushNamed(
+                context,
+                CartPage.routeName,
+              ),
+            ),
+          ],
+        ),
+        body: Card(
+          margin: const EdgeInsets.all(15.0),
+          child: Column(
+              //Padding(  padding: EdgeInsets.only(top: 10.0, left: 15.0),)
+              children: <Widget>[
+                /*
         Container(
           width: 100.0,
           height: 100,
@@ -54,20 +56,87 @@ class PaymentInfoPage extends StatelessWidget {
           )),
         ),*/
 
-            Expanded(
-                child: TextFormField(
-              decoration:
-                  const InputDecoration(labelText: 'Credit Information'),
-              textInputAction: TextInputAction.done,
-              keyboardType: TextInputType.number,
-            )),
-            Expanded(
-                child: TextFormField(
-              decoration: const InputDecoration(labelText: 'Security Code'),
-              textInputAction: TextInputAction.done,
-              keyboardType: TextInputType.number,
-            ))
-          ]),
-    );
+                Padding(
+                    padding: EdgeInsets.all(15.0),
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                          labelText: 'Credit Information'),
+                      textInputAction: TextInputAction.done,
+                      keyboardType: TextInputType.number,
+                    )),
+                Padding(
+                    padding: EdgeInsets.all(15.0),
+                    child: TextFormField(
+                      decoration:
+                          const InputDecoration(labelText: 'Security Code'),
+                      textInputAction: TextInputAction.done,
+                      keyboardType: TextInputType.number,
+                    )),
+                Padding(
+                    padding: EdgeInsets.all(15.0),
+                    child: TextFormField(
+                      decoration:
+                          const InputDecoration(labelText: 'Address Line 1'),
+                      textInputAction: TextInputAction.done,
+                      keyboardType: TextInputType.number,
+                    )),
+                Padding(
+                    padding: EdgeInsets.all(15.0),
+                    child: TextFormField(
+                      decoration:
+                          const InputDecoration(labelText: 'Address Line 2'),
+                      textInputAction: TextInputAction.done,
+                      keyboardType: TextInputType.number,
+                    )),
+                Padding(
+                    padding: EdgeInsets.all(15.0),
+                    child: TextFormField(
+                      decoration: const InputDecoration(labelText: 'State'),
+                      textInputAction: TextInputAction.done,
+                      keyboardType: TextInputType.number,
+                    )),
+                Padding(
+                    padding: EdgeInsets.all(15.0),
+                    child: TextFormField(
+                      decoration: const InputDecoration(labelText: 'City'),
+                      textInputAction: TextInputAction.done,
+                      keyboardType: TextInputType.number,
+                    )),
+                Padding(
+                    padding: EdgeInsets.all(15.0),
+                    child: TextFormField(
+                      decoration:
+                          const InputDecoration(labelText: 'Expiration Month'),
+                      textInputAction: TextInputAction.done,
+                      keyboardType: TextInputType.number,
+                    )),
+                Padding(
+                    padding: EdgeInsets.all(15.0),
+                    child: TextFormField(
+                      decoration:
+                          const InputDecoration(labelText: 'Expiration Year'),
+                      textInputAction: TextInputAction.done,
+                      keyboardType: TextInputType.number,
+                    )),
+                Row(
+                  children: [
+                    IconButton(
+                      icon: const Icon(Icons.arrow_back),
+                      onPressed: () => Navigator.pushNamed(
+                        context,
+                        PaymentInfoPage.routeName,
+                      ),
+                    ),
+                    const Spacer(),
+                    TextButton(
+                        onPressed: () => Navigator.pushNamed(
+                              context,
+                              PaymentInfoPage.routeName,
+                            ),
+                        child: const Text('Purchase')),
+                  ],
+                )
+              ]),
+        ));
   }
 }
